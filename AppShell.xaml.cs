3c04d02bb1c9754e5fee0c778de6b33018431b8a
Kelvin R. Tobias <rt12690@gmail.com>
2025-11-16 14:37:19 -0500

    test

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -5,6 +5,15 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
+		RegisterRoutes();
+	}
+
+	private void RegisterRoutes()
+	{
+		Routing.RegisterRoute(nameof(Views.CourseListPage), typeof(Views.CourseListPage));
+		Routing.RegisterRoute(nameof(Views.TermDetailPage), typeof(Views.TermDetailPage));
+		Routing.RegisterRoute(nameof(Views.CourseDetailPage), typeof(Views.CourseDetailPage));
+		Routing.RegisterRoute(nameof(Views.AssessmentsPage), typeof(Views.AssessmentsPage));
 	}
 }
 
